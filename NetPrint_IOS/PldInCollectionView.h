9591d6292d4175737f431a7c541081ec0f2f88d2
xiaozy <xiaozy@163.com>
2015-08-21 17:29:21 +0800

    -

---
+++ NetPrint_IOS/PldInCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  PldInCollectionView.h
+//  NetPrint_IOS
+//
+//  Created by sjkysjky on 15/8/21.
+//  Copyright (c) 2015年 世纪开元. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PldInCollectionView : UIView
+
+@end
---
+++ NetPrint_IOS/PldInCollectionView.m
@@ -0,0 +1,21 @@
+//
+//  PldInCollectionView.m
+//  NetPrint_IOS
+//
+//  Created by sjkysjky on 15/8/21.
+//  Copyright (c) 2015年 世纪开元. All rights reserved.
+//
+
+#import "PldInCollectionView.h"
+
+@implementation PldInCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
